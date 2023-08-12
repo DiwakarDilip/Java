a4de7369e02938aab44ac78d40692f0c59f533e8
Diwakar <131990141+DiwakarDilip@users.noreply.github.com>
2023-08-12 11:46:44 +0530

    Update pattern.java

--- program/com/pattern.java
+++ program/com/pattern.java
@@ -6,17 +6,17 @@ public static void main(String[] args) {
 	for(int i=1;i<=5;i++)
 	{
 		for(int j=1;j<=i;j++)		  //*
-		{							  //**
+		{				  //**
 			System.out.print("*");	  //***
-		}							  //****
+		}				  //****
 		System.out.println();		  //*****
 	}
 //**********************************************	
 
 	for(int i=1;i<=5;i++)			//	1
-	{								//	22
+	{					//	22
 		for(int j=1;j<=i;j++)		//	333
-		{							//	4444
+		{				//	4444
 			System.out.print(i);	//	55555
 		}
 		System.out.println();
@@ -24,10 +24,10 @@ public static void main(String[] args) {
 //**************************************************	
 	
 	for(int x=1;x<=5;x++) 
-	{								//	1
-		int num=1;					//	12
+	{					//	1
+		int num=1;			//	12
 		for(int y=1;y<=x;y++)		//	123
-		{							//	1234
+		{				//	1234
 			System.out.print(num);	//	12345
 			num++;
 		}
